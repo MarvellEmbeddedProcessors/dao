39531ffdd04c3d6b11caf95b0432a21bcccc0a26
Ashwin Sekhar T K <asekhar@marvell.com>
2024-04-23 12:03:34 +0530

    ci: backport build script updates from dao-devel
     * add additional options to build script
     * updated klocwork license server URL for klocwork 23.4.
    
    Signed-off-by: Ashwin Sekhar T K <asekhar@marvell.com>
    Change-Id: I86e2e707a588d7ff80ee3802d0f03090de730f6c
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpu-offload/+/130969
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Harman Kalra <hkalra@marvell.com>

--- ci/klocwork/klocwork.sh
+++ ci/klocwork/klocwork.sh
@@ -6,7 +6,6 @@
 set -euo pipefail
 
 function help() {
-	set +x
 	echo "Klocwork Check"
 	echo ""
 	echo "Usage:"
@@ -22,7 +21,6 @@ function help() {
 	echo "--jobs | -j                  : Number of parallel jobs [Default: 4]"
 	echo "--project-root | -p          : dpu-offload Project root [Default: PWD]"
 	echo "--help | -h                  : Print this help and exit"
-	set -x
 }
 
 SCRIPT_NAME="$(basename "$0")"
@@ -81,7 +79,7 @@ PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$DEPS_DIR/lib/pkgconfig meson $BUILD_DIR --cros
 
 rm -rf .kwlp .kwps
 kwcheck create
-kwcheck set license.host=llic5-02.marvell.com license.port=33138
+kwcheck set license.host=llic5-01.marvell.com license.port=33138
 
 # List of directories to ignore in klocwork checks
 IGNORE_FILES=""
