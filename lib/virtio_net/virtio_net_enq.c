MarvellEmbeddedProcessors/dao

commit e3c5c741e975ceb7001875428b804e2437f239de

virtio: update hash report based on packet type
This patch changes the code to update hash report
based on packet type.

Signed-off-by: Srujana Challa <schalla@marvell.com>
Change-Id: I5eb40608c524fb060688ef55f371eefcffd82c9e
Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpu-offload/+/130025
Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
Reviewed-by: Nithin Kumar Dabilpuram <ndabilpuram@marvell.com>

lib/virtio_net/virtio_net_enq.c
@@ -121,11 +121,20 @@ process_enq_compl(struct virtio_net_queue *q, struct dao_dma_vchan_state *mem2de
 	}
 }
 
+static __rte_always_inline uint16_t
+mbuf_pkt_type_to_virtio_hash_report(uint8_t *hash_report, uint32_t packet_type)
+{
+	uint32_t index = (packet_type & (RTE_PTYPE_L3_MASK | RTE_PTYPE_L4_MASK)) >> 4;
+
+	return (uint16_t)hash_report[index];
+}
+
 static __rte_always_inline int
 push_enq_data(struct virtio_net_queue *q, struct dao_dma_vchan_state *mem2dev,
 	      struct rte_mbuf **mbufs, uint16_t nb_mbufs, const uint16_t flags)
 {
-	uint64x2_t rss0, rss1, rss2, rss3, d01, d23, rss0213;
+	uint64x2_t rss0, rss1, rss2, rss3, d01, d23, rss0213, h0213;
+	uint32_t pkt_typ0, pkt_typ1, pkt_typ2, pkt_typ3;
 	uint64x2_t flags01, flags23, len01, len23;
 	struct rte_mbuf **mbuf_arr = q->mbuf_arr;
 	uint64_t *mbuf0, *mbuf1, *mbuf2, *mbuf3;
@@ -142,9 +151,8 @@ push_enq_data(struct virtio_net_queue *q, struct dao_dma_vchan_state *mem2dev,
 	uint16_t sd_off, avail_sd, avail_mbuf;
 	uint32x4_t ol_flags, xlen, ylen;
 	uint64x2_t xflags01, xflags23;
+	uint8_t *hrp = q->hash_report;
 	uint64x2_t vdst[4], vsrc[4];
-	uint32x4_t hash_f, hash_rpt;
-	uint32_t rss_hf = q->rss_hf;
 	struct virtio_net_hdr *hdr;
 	uint64x2_t xtmp0, xtmp1;
 	uint16_t used = 0, i = 0;
@@ -294,25 +302,37 @@ push_enq_data(struct virtio_net_queue *q, struct dao_dma_vchan_state *mem2dev,
 		}
 
 		if (flags & VIRTIO_NET_ENQ_OFFLOAD_HASH_REPORT) {
+			pkt_typ0 = *((uint32_t *)mbuf0 + 8);
+			pkt_typ1 = *((uint32_t *)mbuf1 + 8);
+			pkt_typ2 = *((uint32_t *)mbuf2 + 8);
+			pkt_typ3 = *((uint32_t *)mbuf3 + 8);
+
 			rss0 = vld1q_u32((uint32_t *)mbuf0 + 11);
 			rss1 = vld1q_u32((uint32_t *)mbuf1 + 11);
 			rss2 = vld1q_u32((uint32_t *)mbuf2 + 11);
 			rss3 = vld1q_u32((uint32_t *)mbuf3 + 11);
 
+			h0213 = vsetq_lane_u32(mbuf_pkt_type_to_virtio_hash_report(hrp, pkt_typ0),
+					       h0213, 0);
+			h0213 = vsetq_lane_u32(mbuf_pkt_type_to_virtio_hash_report(hrp, pkt_typ1),
+					       h0213, 2);
+			h0213 = vsetq_lane_u32(mbuf_pkt_type_to_virtio_hash_report(hrp, pkt_typ2),
+					       h0213, 1);
+			h0213 = vsetq_lane_u32(mbuf_pkt_type_to_virtio_hash_report(hrp, pkt_typ3),
+					       h0213, 3);
+
 			d01 = vzip1q_u64(rss0, rss1);
 			d23 = vzip1q_u64(rss2, rss3);
-
 			/* d01 elements are stored in even places  for transposet instr*/
 			/* d23 elements are stored in odd places  for transposet instr*/
 			rss0213 = vtrn1q_u32(d01, d23);
 
 			const uint32x4_t def_hash_val = vdupq_n_u32(0);
 
-			hash_f = vdupq_n_u32(rss_hf);
-			hash_rpt = vandq_u32(vcgtq_u32(rss0213, def_hash_val), hash_f);
+			h0213 = vandq_u32(vcgtq_u32(rss0213, def_hash_val), h0213);
 
-			d01 = vtrn1q_u32(rss0213, hash_rpt);
-			d23 = vtrn2q_u32(rss0213, hash_rpt);
+			d01 = vtrn1q_u32(rss0213, h0213);
+			d23 = vtrn2q_u32(rss0213, h0213);
 
 			*(uint64_t *)((uint32_t *)data0 + 3) = vgetq_lane_u64(d01, 0);
 			*(uint64_t *)((uint32_t *)data1 + 3) = vgetq_lane_u64(d01, 1);
@@ -415,8 +435,13 @@ push_enq_data(struct virtio_net_queue *q, struct dao_dma_vchan_state *mem2dev,
 		}
 
 		if (flags & VIRTIO_NET_ENQ_OFFLOAD_HASH_REPORT) {
+			uint16_t hash_rpt;
+
+			hash_rpt =
+				mbuf_pkt_type_to_virtio_hash_report(hrp, *((uint32_t *)mbuf0 + 8));
+
 			hdr->hash_value = ((struct rte_mbuf *)mbuf0)->hash.rss;
-			hdr->hash_report = hdr->hash_value ? rss_hf : 0;
+			hdr->hash_report = hdr->hash_value ? hash_rpt : 0;
 		}
 
 		d_flags = *DESC_PTR_OFF(sd_desc_base, off, 8);

app/virtio-l2fwd/virtio_l2fwd.c
@@ -1438,6 +1438,8 @@ reconfig_ethdev(uint16_t portid, uint16_t q_count)
 	struct rte_eth_conf *local_port_conf;
 	struct rte_eth_dev_info dev_info;
 	struct rte_eth_txconf *txconf;
+	uint64_t feature_bits;
+	uint16_t virtio_devid;
 	uint16_t nb_rx_queue;
 	uint32_t nb_tx_queue;
 	uint16_t queueid;
@@ -1494,6 +1496,16 @@ reconfig_ethdev(uint16_t portid, uint16_t q_count)
 
 	rss_table_reset(portid);
 	eth_dev_q_count[portid] = q_count;
+
+	virtio_devid = eth_map[portid].id;
+	feature_bits = dao_virtio_netdev_feature_bits_get(virtio_devid);
+	if (feature_bits & RTE_BIT64(VIRTIO_NET_F_HASH_REPORT)) {
+		rc = rte_eth_dev_set_ptypes(portid, RTE_PTYPE_L3_MASK | RTE_PTYPE_L4_MASK, NULL, 0);
+		if (rc < 0) {
+			APP_ERR("rte_eth_dev_start: err=%d, port=%d\n", rc, portid);
+			return rc;
+		}
+	}
 
 	rc = rte_eth_dev_start(portid);
 	if (rc < 0) {
@@ -1968,7 +1980,8 @@ hash_report_enable(uint16_t virtio_devid)
 	uint16_t virt_q_count, portid;
 	int rc;
 
-	enable_hash_report = dao_virtio_netdev_feature_bits_get(virtio_devid) & 0x200000000000000;
+	enable_hash_report = dao_virtio_netdev_feature_bits_get(virtio_devid) &
+			     RTE_BIT64(VIRTIO_NET_F_HASH_REPORT);
 
 	portid = virtio_map[virtio_devid].id;
 	local_port_conf = &eth_dev_conf[portid];
@@ -1977,7 +1990,7 @@ hash_report_enable(uint16_t virtio_devid)
 	rx_offloads = local_port_conf->rxmode.offloads;
 
 	rx_offloads &= ~(RTE_ETH_RX_OFFLOAD_RSS_HASH);
-	if (enable_hash_report & RTE_BIT64(VIRTIO_NET_F_HASH_REPORT))
+	if (enable_hash_report)
 		rx_offloads |= RTE_ETH_RX_OFFLOAD_RSS_HASH;
 
 	if (local_port_conf->rxmode.offloads == rx_offloads) {

lib/virtio/spec/virtio_net.h
@@ -107,6 +107,16 @@ struct virtio_net_hdr {
 	rte_le16_t num_buffers;
 	/** hash value of packet */
 	rte_le32_t hash_value;
+#define VIRTIO_NET_HASH_REPORT_NONE     0
+#define VIRTIO_NET_HASH_REPORT_IPv4     1
+#define VIRTIO_NET_HASH_REPORT_TCPv4    2
+#define VIRTIO_NET_HASH_REPORT_UDPv4    3
+#define VIRTIO_NET_HASH_REPORT_IPv6     4
+#define VIRTIO_NET_HASH_REPORT_TCPv6    5
+#define VIRTIO_NET_HASH_REPORT_UDPv6    6
+#define VIRTIO_NET_HASH_REPORT_IPv6_EX  7
+#define VIRTIO_NET_HASH_REPORT_TCPv6_EX 8
+#define VIRTIO_NET_HASH_REPORT_UDPv6_EX 9
 	/** hash report of packet */
 	rte_le16_t hash_report;
 	/** padding reserved of packet */

lib/virtio_net/dao_virtio_netdev.h
@@ -82,6 +82,7 @@ struct dao_virtio_netdev {
 	uint16_t enq_fn_id;
 	/** Descriptors management function id */
 	uint16_t mgmt_fn_id;
+	/** Table to find hash report based on packet type */
 #define DAO_VIRTIO_NETDEV_MEM_SZ 8192
 	uint8_t reserved[DAO_VIRTIO_NETDEV_MEM_SZ];
 };

lib/virtio_net/virtio_net_priv.h
@@ -16,11 +16,11 @@ struct virtio_net_queue {
 	uint16_t netdev_id;
 	uint8_t virtio_hdr_sz;
 	uint8_t auto_free;
+	uint8_t *hash_report;
 
 	/* Slow path */
 	struct dao_virtio_netdev *dao_netdev __rte_cache_aligned;
 	uint16_t qid;
-	uint32_t rss_hf;
 
 	/* Read-Write worker. */
 	uint16_t pend_sd_mbuf __rte_cache_aligned;
@@ -70,6 +70,8 @@ struct virtio_netdev {
 	bool auto_free_en;
 	uint16_t reta_size;
 	uint16_t hash_key_size;
+#define DAO_HASH_REPORT_INDEX_MAX 256
+	uint8_t *hash_report;
 
 	/* Fast path data */
 	struct virtio_net_queue *qs[DAO_VIRTIO_MAX_QUEUES] __rte_cache_aligned;

lib/virtio_net/virtio_netdev.c
@@ -20,14 +20,104 @@ dao_net_desc_manage_fn_t dao_net_desc_manage_fns[VIRTIO_NET_DESC_MANAGE_LAST << 
 struct dao_virtio_netdev_cbs user_cbs;
 int virtio_netdev_clear_queue_info(struct virtio_netdev *netdev);
 
+static void
+virtio_hash_types_to_hash_report(struct virtio_netdev *netdev, uint64_t virtio_hash_types)
+{
+	uint8_t *hash_report = netdev->hash_report;
+
+	memset(hash_report, 0, sizeof(uint8_t) * DAO_HASH_REPORT_INDEX_MAX);
+	/* IPv4 hash report */
+	if (virtio_hash_types & VIRTIO_NET_HASH_TYPE_IPV4) {
+		hash_report[RTE_PTYPE_L3_IPV4 >> 4] = VIRTIO_NET_HASH_REPORT_IPv4;
+		hash_report[RTE_PTYPE_L3_IPV4_EXT >> 4] = VIRTIO_NET_HASH_REPORT_IPv4;
+		hash_report[RTE_PTYPE_L3_IPV4_EXT_UNKNOWN >> 4] = VIRTIO_NET_HASH_REPORT_IPv4;
+		hash_report[(RTE_PTYPE_L3_IPV4 | RTE_PTYPE_L4_TCP) >> 4] =
+			VIRTIO_NET_HASH_REPORT_IPv4;
+		hash_report[(RTE_PTYPE_L3_IPV4 | RTE_PTYPE_L4_UDP) >> 4] =
+			VIRTIO_NET_HASH_REPORT_IPv4;
+	}
+
+	/* TCPv4 hash report */
+	if (virtio_hash_types & VIRTIO_NET_HASH_TYPE_TCPV4)
+		hash_report[(RTE_PTYPE_L3_IPV4 | RTE_PTYPE_L4_TCP) >> 4] =
+			VIRTIO_NET_HASH_REPORT_TCPv4;
+
+	/* UDPv4 hash report */
+	if (virtio_hash_types & VIRTIO_NET_HASH_TYPE_UDPV4)
+		hash_report[(RTE_PTYPE_L3_IPV4 | RTE_PTYPE_L4_UDP) >> 4] =
+			VIRTIO_NET_HASH_REPORT_UDPv4;
+
+	/* IPv6 hash report */
+	if (virtio_hash_types & VIRTIO_NET_HASH_TYPE_IPV6) {
+		hash_report[RTE_PTYPE_L3_IPV6 >> 4] = VIRTIO_NET_HASH_REPORT_IPv6;
+		hash_report[RTE_PTYPE_L3_IPV6_EXT >> 4] = VIRTIO_NET_HASH_REPORT_IPv6;
+		hash_report[RTE_PTYPE_L3_IPV6_EXT_UNKNOWN >> 4] = VIRTIO_NET_HASH_REPORT_IPv6;
+		hash_report[(RTE_PTYPE_L3_IPV6 | RTE_PTYPE_L4_TCP) >> 4] =
+			VIRTIO_NET_HASH_REPORT_IPv6;
+		hash_report[(RTE_PTYPE_L3_IPV6 | RTE_PTYPE_L4_UDP) >> 4] =
+			VIRTIO_NET_HASH_REPORT_IPv6;
+		hash_report[(RTE_PTYPE_L3_IPV6_EXT_UNKNOWN | RTE_PTYPE_L4_TCP) >> 4] =
+			VIRTIO_NET_HASH_REPORT_IPv6;
+		hash_report[(RTE_PTYPE_L3_IPV6_EXT | RTE_PTYPE_L4_UDP) >> 4] =
+			VIRTIO_NET_HASH_REPORT_IPv6;
+	}
+
+	/* TCPv6 hash report */
+	if (virtio_hash_types & VIRTIO_NET_HASH_TYPE_TCPV6) {
+		hash_report[(RTE_PTYPE_L3_IPV6 | RTE_PTYPE_L4_TCP) >> 4] =
+			VIRTIO_NET_HASH_REPORT_TCPv6;
+		hash_report[(RTE_PTYPE_L3_IPV6_EXT_UNKNOWN | RTE_PTYPE_L4_TCP) >> 4] =
+			VIRTIO_NET_HASH_REPORT_TCPv6;
+		hash_report[(RTE_PTYPE_L3_IPV6_EXT | RTE_PTYPE_L4_TCP) >> 4] =
+			VIRTIO_NET_HASH_REPORT_TCPv6;
+	}
+
+	/* UDPv6 hash report */
+	if (virtio_hash_types & VIRTIO_NET_HASH_TYPE_UDPV6) {
+		hash_report[(RTE_PTYPE_L3_IPV6 | RTE_PTYPE_L4_UDP) >> 4] =
+			VIRTIO_NET_HASH_REPORT_UDPv6;
+		hash_report[(RTE_PTYPE_L3_IPV6_EXT_UNKNOWN | RTE_PTYPE_L4_UDP) >> 4] =
+			VIRTIO_NET_HASH_REPORT_UDPv6;
+		hash_report[(RTE_PTYPE_L3_IPV6_EXT | RTE_PTYPE_L4_UDP) >> 4] =
+			VIRTIO_NET_HASH_REPORT_UDPv6;
+	}
+
+	/* IPv6 EXT hash report */
+	if (virtio_hash_types & VIRTIO_NET_HASH_TYPE_IP_EX) {
+		hash_report[RTE_PTYPE_L3_IPV6_EXT >> 4] = VIRTIO_NET_HASH_REPORT_IPv6_EX;
+		hash_report[RTE_PTYPE_L3_IPV6_EXT_UNKNOWN >> 4] = VIRTIO_NET_HASH_REPORT_IPv6_EX;
+		hash_report[(RTE_PTYPE_L3_IPV6_EXT | RTE_PTYPE_L4_TCP) >> 4] =
+			VIRTIO_NET_HASH_REPORT_IPv6_EX;
+		hash_report[(RTE_PTYPE_L3_IPV6_EXT_UNKNOWN | RTE_PTYPE_L4_TCP) >> 4] =
+			VIRTIO_NET_HASH_REPORT_IPv6_EX;
+		hash_report[(RTE_PTYPE_L3_IPV6_EXT | RTE_PTYPE_L4_UDP) >> 4] =
+			VIRTIO_NET_HASH_REPORT_IPv6_EX;
+		hash_report[(RTE_PTYPE_L3_IPV6_EXT_UNKNOWN | RTE_PTYPE_L4_UDP) >> 4] =
+			VIRTIO_NET_HASH_REPORT_IPv6_EX;
+	}
+
+	/* TCPv6 EXT hash report */
+	if (virtio_hash_types & VIRTIO_NET_HASH_TYPE_TCP_EX) {
+		hash_report[(RTE_PTYPE_L3_IPV6_EXT | RTE_PTYPE_L4_TCP) >> 4] =
+			VIRTIO_NET_HASH_REPORT_TCPv6_EX;
+		hash_report[(RTE_PTYPE_L3_IPV6_EXT_UNKNOWN | RTE_PTYPE_L4_TCP) >> 4] =
+			VIRTIO_NET_HASH_REPORT_TCPv6_EX;
+	}
+
+	/* UDPv6 EXT hash report */
+	if (virtio_hash_types & VIRTIO_NET_HASH_TYPE_UDP_EX) {
+		hash_report[(RTE_PTYPE_L3_IPV6_EXT | RTE_PTYPE_L4_UDP) >> 4] =
+			VIRTIO_NET_HASH_REPORT_UDPv6_EX;
+		hash_report[(RTE_PTYPE_L3_IPV6_EXT_UNKNOWN | RTE_PTYPE_L4_UDP) >> 4] =
+			VIRTIO_NET_HASH_REPORT_UDPv6_EX;
+	}
+}
+
 static int
 net_rss_setup(struct virtio_netdev *netdev, struct virtio_net_ctrl *ctrl_cmd)
 {
 	struct virtio_net_ctrl_rss *rss = (struct virtio_net_ctrl_rss *)ctrl_cmd->data;
-	uint32_t max_vqs = netdev->dev.max_virtio_queues - 1;
-	struct virtio_net_queue *queue;
-	uint32_t i;
-	int rc;
+	int ret;
 
 	if (user_cbs.rss_cb == NULL)
 		return -ENOTSUP;
@@ -43,16 +133,14 @@ net_rss_setup(struct virtio_netdev *netdev, struct virtio_net_ctrl *ctrl_cmd)
 	/* Update the core map to requested number of queues and
 	 * configure rss.
 	 */
-	rc = user_cbs.rss_cb(netdev->dev.dev_id, rss);
-	if (!rc) {
-		for (i = 0; i < max_vqs; i++) {
-			queue = netdev->qs[i];
-			if (queue)
-				queue->rss_hf = rss->hash_types;
-		}
-	}
+	ret = user_cbs.rss_cb(netdev->dev.dev_id, rss);
+	if (!ret)
+		/* Set hash report values based on the requested hash types, which will be
+		 * used in enqueue data path.
+		 */
+		virtio_hash_types_to_hash_report(netdev, rss->hash_types);
 
-	return rc;
+	return ret;
 }
 
 static int
@@ -248,6 +336,7 @@ virtio_netdev_populate_queue_info(struct virtio_netdev *netdev, uint16_t queue_i
 	dao_netdev->qs[queue_id] = queue;
 	queue->dao_netdev = dao_netdev;
 	queue->netdev_id = netdev->dev.dev_id;
+	queue->hash_report = netdev->hash_report;
 
 	if (dev->feature_bits & RTE_BIT64(VIRTIO_NET_F_HASH_REPORT))
 		queue->virtio_hdr_sz =
@@ -642,6 +731,12 @@ dao_virtio_netdev_init(uint16_t devid, struct dao_virtio_netdev_conf *conf)
 	if (conf->flags & DAO_VIRTIO_NETDEV_EXTBUF)
 		virtio_netdev->mgmt_fn_id |= VIRTIO_NET_DESC_MANAGE_EXTBUF;
 
+	netdev->hash_report = rte_zmalloc(NULL, sizeof(uint8_t) * DAO_HASH_REPORT_INDEX_MAX, 0);
+	if (!netdev->hash_report) {
+		dao_err("[dev %u] Failed to allocate memory for hash report table", dev->dev_id);
+		return -ENOMEM;
+	}
+
 	/* One time setup */
 	dev_cbs[VIRTIO_DEV_TYPE_NET].dev_status = virtio_netdev_status_cb;
 	dev_cbs[VIRTIO_DEV_TYPE_NET].cq_cmd_process = virtio_netdev_cq_cmd_process;
@@ -656,6 +751,7 @@ dao_virtio_netdev_fini(uint16_t devid)
 	struct dao_virtio_netdev *virtio_netdev = &dao_virtio_netdevs[devid];
 	struct virtio_netdev *netdev = virtio_netdev_priv(virtio_netdev);
 
+	rte_free(netdev->hash_report);
 	return virtio_dev_fini(&netdev->dev);
 }
 

commit fc1af3b98b8e444c1ddd7eee9fb215bd57fef85e

virtio_net: fix build issue with gcc-13 compiler
This patch fixes build error with below commit.

Fixes: e3c5c741e975 ("virtio: update hash report based on packet type")

Signed-off-by: Srujana Challa <schalla@marvell.com>
Change-Id: I8aefb42de4a552f374a3aa251893003f2b146335
Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpu-offload/+/131565
Reviewed-by: Jerin Jacob <jerinj@marvell.com>
Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>

lib/virtio_net/virtio_net_enq.c
@@ -133,7 +133,7 @@ static __rte_always_inline int
 push_enq_data(struct virtio_net_queue *q, struct dao_dma_vchan_state *mem2dev,
 	      struct rte_mbuf **mbufs, uint16_t nb_mbufs, const uint16_t flags)
 {
-	uint64x2_t rss0, rss1, rss2, rss3, d01, d23, rss0213, h0213;
+	uint64x2_t rss0, rss1, rss2, rss3, d01, d23, rss0213;
 	uint32_t pkt_typ0, pkt_typ1, pkt_typ2, pkt_typ3;
 	uint64x2_t flags01, flags23, len01, len23;
 	struct rte_mbuf **mbuf_arr = q->mbuf_arr;
@@ -149,7 +149,7 @@ push_enq_data(struct virtio_net_queue *q, struct dao_dma_vchan_state *mem2dev,
 	uint64x2_t len_olflags0, len_olflags1;
 	uint64x2_t len_olflags2, len_olflags3;
 	uint16_t sd_off, avail_sd, avail_mbuf;
-	uint32x4_t ol_flags, xlen, ylen;
+	uint32x4_t ol_flags, xlen, ylen, h0213;
 	uint64x2_t xflags01, xflags23;
 	uint8_t *hrp = q->hash_report;
 	uint64x2_t vdst[4], vsrc[4];
@@ -312,6 +312,7 @@ push_enq_data(struct virtio_net_queue *q, struct dao_dma_vchan_state *mem2dev,
 			rss2 = vld1q_u32((uint32_t *)mbuf2 + 11);
 			rss3 = vld1q_u32((uint32_t *)mbuf3 + 11);
 
+			h0213 = vdupq_n_u32(0);
 			h0213 = vsetq_lane_u32(mbuf_pkt_type_to_virtio_hash_report(hrp, pkt_typ0),
 					       h0213, 0);
 			h0213 = vsetq_lane_u32(mbuf_pkt_type_to_virtio_hash_report(hrp, pkt_typ1),