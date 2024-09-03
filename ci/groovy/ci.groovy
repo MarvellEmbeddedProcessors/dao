484fea29a3b9d2911dcf8f86a33b3e2ab1a8ce4e
Ashwin Sekhar T K <asekhar@marvell.com>
2024-09-03 06:48:31 +0000

    ci: change directory name
    Change directory name.
    
    Signed-off-by: Ashwin Sekhar T K <asekhar@marvell.com>
    Change-Id: I1b72ffb311e20fe18de6ef6df65e99629a975d29
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpu-offload/+/134770
    Reviewed-by: Jerin Jacob <jerinj@marvell.com>
    Tested-by: Jerin Jacob <jerinj@marvell.com>

--- ci/groovy/ci.groovy
+++ ci/groovy/ci.groovy
@@ -4,7 +4,7 @@
 
 def run_ci(Object s) {
 	node ("buildenv-2004-le") {
-		s.GROOVY_DIR = '/data/isoc_platform_devops/dev-ci/scripts_cache/dev-ci/dpu-offload'
+		s.GROOVY_DIR = '/data/isoc_platform_devops/dev-ci/scripts_cache/dev-ci/dao'
 		s.preinit = load s.GROOVY_DIR + "/preinit.groovy"
 	}
 	s.preinit.run(s)
