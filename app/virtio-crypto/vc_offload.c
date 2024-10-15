7446eecb006d01802f7cad15ece2ada6ddc54047
Gowrishankar Muthukrishnan <gmuthukrishn@marvell.com>
2024-10-15 12:35:50 +0000

    app/virtio-crypto: add lcore checks
    Add routines to check lcore config.
    
    Signed-off-by: Anoob Joseph <anoobj@marvell.com>
    Signed-off-by: Gowrishankar Muthukrishnan <gmuthukrishn@marvell.com>
    Change-Id: Ie90faee06acb1175c7e0be1abb845af535fdb48a
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpu-offload/+/137236
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Jerin Jacob <jerinj@marvell.com>

--- app/virtio-crypto/vc_offload.c
+++ app/virtio-crypto/vc_offload.c
@@ -27,6 +27,28 @@ uint64_t lcore_crypto_mask[RTE_CRYPTO_MAX_DEVS];
 
 static volatile bool force_quit;
 
+static bool
+is_virtio_dev_enabled(uint16_t virtio_devid)
+{
+	uint64_t i = virtio_devid / 64;
+	uint64_t j = virtio_devid % 64;
+
+	if (i > 1)
+		return false;
+	return virtio_mask_ena[i] & RTE_BIT64(j);
+}
+
+static bool
+is_crypto_dev_enabled(uint16_t crypto_devid)
+{
+	uint64_t i = crypto_devid % 64;
+
+	if (crypto_devid >= RTE_CRYPTO_MAX_DEVS)
+		return false;
+
+	return crypto_mask_ena & RTE_BIT64(i);
+}
+
 static void
 signal_handler(int signum)
 {
@@ -35,6 +57,55 @@ signal_handler(int signum)
 		APP_INFO("Signal %d received, preparing to exit...\n", signum);
 		force_quit = true;
 	}
+}
+
+static int
+check_lcore_params(void)
+{
+	uint8_t lcore;
+	uint16_t i;
+
+	for (i = 0; i < DAO_VIRTIO_DEV_MAX; ++i) {
+		if (!is_virtio_dev_enabled(i))
+			continue;
+
+		for (lcore = 0; lcore < RTE_MAX_LCORE; lcore++) {
+			if (!(RTE_BIT64(lcore) & lcore_virtio_mask[i]))
+				continue;
+
+			if (!rte_lcore_is_enabled(lcore)) {
+				APP_ERR("Error: lcore %hhu is not enabled in lcore mask\n", lcore);
+				return -1;
+			}
+
+			if (lcore == rte_get_main_lcore()) {
+				APP_ERR("Error: lcore %u is main lcore\n", lcore);
+				return -1;
+			}
+		}
+	}
+
+	for (i = 0; i < RTE_CRYPTO_MAX_DEVS; ++i) {
+		if (!is_crypto_dev_enabled(i))
+			continue;
+
+		for (lcore = 0; lcore < RTE_MAX_LCORE; lcore++) {
+			if (!(RTE_BIT64(lcore) & lcore_crypto_mask[i]))
+				continue;
+
+			if (!rte_lcore_is_enabled(lcore)) {
+				APP_ERR("Error: lcore %hhu is not enabled in lcore mask\n", lcore);
+				return -1;
+			}
+
+			if (lcore == rte_get_main_lcore()) {
+				APP_ERR("Error: lcore %u is main lcore\n", lcore);
+				return -1;
+			}
+		}
+	}
+
+	return 0;
 }
 
 int
@@ -56,6 +127,9 @@ main(int argc, char **argv)
 	rc = parse_args(argc, argv);
 	if (rc < 0)
 		rte_exit(EXIT_FAILURE, "Invalid VC offload parameters\n");
+
+	if (check_lcore_params() < 0)
+		rte_exit(EXIT_FAILURE, "Invalid lcore params\n");
 
 	rte_eal_cleanup();
 
