a001ce1819594d61ba2cebfe1b6e6efc8fc00398
Gowrishankar Muthukrishnan <gmuthukrishn@marvell.com>
2024-10-15 17:12:57 +0000

    app/virtio-crypto: add device lcore mapping
    Add device-lcore mapping.
    
    Signed-off-by: Anoob Joseph <anoobj@marvell.com>
    Signed-off-by: Gowrishankar Muthukrishnan <gmuthukrishn@marvell.com>
    Change-Id: Ib4706f8fc310a6365585f255e0841f4080e01b27
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpu-offload/+/137237
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Jerin Jacob <jerinj@marvell.com>

--- app/virtio-crypto/vc_offload.c
+++ app/virtio-crypto/vc_offload.c
@@ -7,7 +7,9 @@
 #include <stdlib.h>
 
 #include <rte_common.h>
+#include <rte_cryptodev.h>
 #include <rte_eal.h>
+#include <rte_graph.h>
 #include <rte_log.h>
 
 #include <dao_virtio.h>
@@ -24,6 +26,37 @@ uint64_t lcore_virtio_mask[DAO_VIRTIO_DEV_MAX];
 uint64_t crypto_mask_ena;
 uint16_t nb_cryptodevs;
 uint64_t lcore_crypto_mask[RTE_CRYPTO_MAX_DEVS];
+
+#define MAX_VIRTIO_RX_PER_LCORE         128
+#define MAX_VIRTIO_CRYPTO_DEQ_PER_LCORE 1
+
+struct lcore_crypto_deq {
+	uint16_t devid;
+	char node_name[RTE_NODE_NAMESIZE];
+
+	struct vc_cryptodev_deq_node_ctx *cryptodev_deq;
+	struct vc_virtio_tx_node_ctx *virtio_tx;
+};
+
+struct lcore_virtio_rx {
+	uint16_t virtio_devid;
+	char node_name[RTE_NODE_NAMESIZE];
+	struct vc_virtio_rx_node_ctx *virtio_rx;
+	struct vc_cryptodev_enq_node_ctx *cryptodev_enq;
+};
+
+/* Lcore conf */
+struct lcore_conf {
+	/* Fast path accessed */
+
+	uint16_t nb_virtio_rx;
+	struct lcore_virtio_rx virtio_rx[MAX_VIRTIO_RX_PER_LCORE];
+	uint16_t nb_crypto_deq;
+	struct lcore_crypto_deq crypto_deq[MAX_VIRTIO_CRYPTO_DEQ_PER_LCORE];
+
+} __rte_cache_aligned;
+
+static struct lcore_conf lcore_conf[RTE_MAX_LCORE];
 
 static volatile bool force_quit;
 
@@ -108,6 +141,44 @@ check_lcore_params(void)
 	return 0;
 }
 
+static int
+init_lcore_virtio_rx(void)
+{
+	uint16_t nb_crypto_deq, cdev_id = 0;
+	uint16_t virtio_devid, nb_virtio_rx;
+	uint8_t lcore;
+
+	for (virtio_devid = 0; virtio_devid < DAO_VIRTIO_DEV_MAX; ++virtio_devid) {
+		if (!is_virtio_dev_enabled(virtio_devid))
+			continue;
+
+		for (lcore = 0; lcore < RTE_MAX_LCORE; lcore++) {
+			if (!(RTE_BIT64(lcore) & lcore_virtio_mask[virtio_devid]))
+				continue;
+
+			nb_virtio_rx = lcore_conf[lcore].nb_virtio_rx;
+
+			lcore_conf[lcore].virtio_rx[nb_virtio_rx].virtio_devid = virtio_devid;
+			snprintf(lcore_conf[lcore].virtio_rx[nb_virtio_rx].node_name,
+				 RTE_NODE_NAMESIZE, "vc_virtio_rx-%u", virtio_devid);
+			lcore_conf[lcore].nb_virtio_rx++;
+
+			/* If virtio-dev is enabled, then create crypto dequeue nodes as well. */
+
+			nb_crypto_deq = lcore_conf[lcore].nb_crypto_deq;
+
+			if (nb_crypto_deq == 0) {
+				lcore_conf[lcore].crypto_deq[nb_crypto_deq].devid = cdev_id;
+				snprintf(lcore_conf[lcore].crypto_deq[nb_crypto_deq].node_name,
+					 RTE_NODE_NAMESIZE, "vc_cryptodev_deq-%u", cdev_id);
+				lcore_conf[lcore].nb_crypto_deq = 1;
+			}
+		}
+	}
+
+	return 0;
+}
+
 int
 main(int argc, char **argv)
 {
@@ -130,6 +201,9 @@ main(int argc, char **argv)
 
 	if (check_lcore_params() < 0)
 		rte_exit(EXIT_FAILURE, "Invalid lcore params\n");
+
+	if (init_lcore_virtio_rx() < 0)
+		rte_exit(EXIT_FAILURE, "Failed to init lcore virtio rx\n");
 
 	rte_eal_cleanup();
 
