c9681ce242bd14453dc6a79d5305b4d3ad0187ef
Anoob Joseph <anoobj@marvell.com>
2025-01-05 16:04:18 +0000

    lib/liquid_crypto: add stub implementation
    Add sample implementation.
    
    Signed-off-by: Anoob Joseph <anoobj@marvell.com>
    Change-Id: I582074434ae1e8d6c122fb601982ebfbebbff6d6
    Reviewed-by: Shijith Thotton <sthotton@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpu-offload/+/147609
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Jerin Jacob <jerinj@marvell.com>

--- lib/liquid_crypto/liquid_crypto_api.c
+++ lib/liquid_crypto/liquid_crypto_api.c
@@ -45,3 +45,79 @@ dao_liquid_crypto_info_get(struct dao_liquid_crypto_info *info)
 
 	return 0;
 }
+
+int
+dao_liquid_crypto_dev_create(uint8_t dev_id, uint16_t nb_qp)
+{
+	/* Call eth TRS API
+	 * - Create the eth device
+	 * rte_eth_dev_configure()
+	 * rte_eth_dev_adjust_nb_rx_tx_desc()
+	 */
+
+	RTE_SET_USED(dev_id);
+	RTE_SET_USED(nb_qp);
+
+	return -ENOTSUP;
+}
+
+int
+dao_liquid_crypto_dev_destroy(uint8_t dev_id)
+{
+	/* Call eth TRS API
+	 * - Destroy the eth device
+	 * rte_eth_dev_close()
+	 */
+
+	RTE_SET_USED(dev_id);
+
+	return -ENOTSUP;
+}
+
+int
+dao_liquid_crypto_qp_configure(uint8_t dev_id, uint16_t qp_id,
+			       struct dao_liquid_crypto_qp_conf *conf)
+
+{
+	/* Create mempool etc */
+
+	/* Call eth TRS API
+	 * - Configure the eth queue pair
+	 * rte_eth_rx_queue_setup()
+	 * rte_eth_tx_queue_setup()
+	 */
+
+	RTE_SET_USED(dev_id);
+	RTE_SET_USED(qp_id);
+	RTE_SET_USED(conf);
+
+	return -ENOTSUP;
+}
+
+int
+dao_liquid_crypto_dev_start(uint8_t dev_id)
+{
+	/* Call eth TRS API
+	 * - Start the eth device
+	 * rte_eth_promiscuous_enable()
+	 * rte_eth_dev_start()
+	 * rte_eth_link_get()
+	 */
+
+	RTE_SET_USED(dev_id);
+
+	return -ENOTSUP;
+}
+
+int
+dao_liquid_crypto_dev_stop(uint8_t dev_id)
+{
+	/* Call eth TRS API
+	 * - Stop the eth device
+	 * rte_eth_dev_stop()
+	 */
+
+	RTE_SET_USED(dev_id);
+
+	return -ENOTSUP;
+}
