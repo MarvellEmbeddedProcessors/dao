ef980338728124c46b7266e5545f9b262f8cb321
Anoob Joseph <anoobj@marvell.com>
2025-01-10 16:55:46 +0000

    lib/liquid_crypto: add qp configure
    Add implementation for qp configure.
    
    Signed-off-by: Anoob Joseph <anoobj@marvell.com>
    Change-Id: Idd1f1d0f642f10d8afa01c50da7283d190147edf
    Reviewed-by: Vidya Sagar Velumuri <vvelumuri@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpu-offload/+/147754
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Jerin Jacob <jerinj@marvell.com>

--- lib/liquid_crypto/liquid_crypto_api.c
+++ lib/liquid_crypto/liquid_crypto_api.c
@@ -5,6 +5,9 @@
 #include <string.h>
 
 #include <rte_eal.h>
+#include <rte_malloc.h>
+#include <rte_mbuf.h>
+#include <rte_mempool.h>
 
 #include <dao_eth_trs.h>
 #include <dao_liquid_crypto.h>
@@ -15,6 +18,9 @@
 static struct dao_liquid_crypto_info liquid_crypto_info;
 
 static struct liquid_crypto_dev liquid_crypto_devs[DAO_CRYPTO_MAX_NB_DEV];
+
+/** Forward declarations */
+static int liquid_crypto_qp_free(uint8_t dev_id, uint16_t qp_id);
 
 int
 dao_liquid_crypto_init(void)
@@ -142,7 +148,7 @@ int
 dao_liquid_crypto_dev_destroy(uint8_t dev_id)
 {
 	struct liquid_crypto_dev *dev;
-	int rc;
+	int rc, i;
 
 	if (dev_id >= liquid_crypto_info.nb_dev) {
 		dao_err("Invalid argument. dev_id must be between 0 and %u.",
@@ -158,6 +164,11 @@ dao_liquid_crypto_dev_destroy(uint8_t dev_id)
 
 	dev = &liquid_crypto_devs[dev_id];
 
+	if (dev->is_created) {
+		for (i = 0; i < dev->nb_qp; i++)
+			liquid_crypto_qp_free(dev_id, i);
+	}
+
 	memset(dev, 0, sizeof(*dev));
 
 	return 0;
@@ -166,21 +177,140 @@ dao_liquid_crypto_dev_destroy(uint8_t dev_id)
 int
 dao_liquid_crypto_qp_configure(uint8_t dev_id, uint16_t qp_id,
 			       struct dao_liquid_crypto_qp_conf *conf)
-
 {
-	/* Create mempool etc */
+	struct dao_eth_trs_queue_config trs_queue_conf;
+	struct dao_eth_trs_info trs_info;
+	char name[RTE_MEMZONE_NAMESIZE];
+	uint16_t nb_desc, max_seg_size;
+	struct liquid_crypto_dev *dev;
+	struct liquid_crypto_qp *qp;
+	struct rte_mempool *mp;
+	int rc;
 
-	/* Call eth TRS API
-	 * - Configure the eth queue pair
-	 * rte_eth_rx_queue_setup()
-	 * rte_eth_tx_queue_setup()
-	 */
+	if (conf == NULL) {
+		dao_err("Invalid argument.");
+		return -EINVAL;
+	}
 
-	RTE_SET_USED(dev_id);
-	RTE_SET_USED(qp_id);
-	RTE_SET_USED(conf);
+	memset(&trs_queue_conf, 0, sizeof(trs_queue_conf));
 
-	return -ENOTSUP;
+	rc = dao_eth_trs_info(&trs_info);
+	if (rc != 0) {
+		dao_err("Could not get ethernet transport information.");
+		return rc;
+	}
+
+	if (conf->nb_desc < trs_info.min_queue_size || conf->nb_desc > trs_info.max_queue_size) {
+		dao_err("Invalid argument. nb_desc must be between %u and %u.",
+			trs_info.min_queue_size, trs_info.max_queue_size);
+		return -EINVAL;
+	}
+
+	if (conf->max_seg_size < trs_info.min_buf_len ||
+	    conf->max_seg_size > trs_info.max_pkt_len) {
+		dao_err("Invalid argument. max_seg_sz must be between %u and %u.",
+			trs_info.min_buf_len, trs_info.max_pkt_len);
+		return -EINVAL;
+	}
+
+	if (trs_info.min_buf_len > LIQUID_CRYPTO_BUF_SZ_MIN) {
+		dao_err("[Internal error] Minimum buffer length exceeds the supported value.");
+		return -EINVAL;
+	}
+
+	dev = &liquid_crypto_devs[dev_id];
+
+	if (!dev->is_created) {
+		dao_err("Invalid device. Device(%d) not created.", dev_id);
+		return -EINVAL;
+	}
+
+	if (qp_id >= dev->nb_qp) {
+		dao_err("Invalid argument. qp_id must be between 0 and %u.", dev->nb_qp - 1);
+		return -EINVAL;
+	}
+
+	snprintf(name, sizeof(name), "liquid_crypto_qp_%u_%u", dev_id, qp_id);
+
+	qp = rte_zmalloc(name, sizeof(*qp), 0);
+	if (qp == NULL) {
+		dao_err("could not allocate memory.");
+		return -ENOMEM;
+	}
+
+	/* Align to the next power of 2 to simplify datapath checks */
+	nb_desc = rte_align32pow2(conf->nb_desc);
+
+	max_seg_size = conf->max_seg_size;
+
+	snprintf(name, sizeof(name), "liquid_crypto_rx_mp_%u_%u", dev_id, qp_id);
+
+	mp = rte_pktmbuf_pool_create(name, nb_desc, 0, 0, max_seg_size, 0);
+	if (mp == NULL) {
+		dao_err("Could not create Rx mbuf pool.");
+		goto qp_free;
+	}
+
+	qp->rx_mp = mp;
+
+	snprintf(name, sizeof(name), "liquid_crypto_tx_mp_%u_%u", dev_id, qp_id);
+
+	mp = rte_pktmbuf_pool_create(name, nb_desc, 0, 0, max_seg_size, 0);
+	if (mp == NULL) {
+		dao_err("Could not create Tx mbuf pool.");
+		goto rx_mp_free;
+	}
+
+	qp->tx_mp = mp;
+
+	trs_queue_conf.rx_mp = qp->rx_mp;
+	trs_queue_conf.queue_size = nb_desc;
+	rc = dao_eth_trs_dev_queue_configure(dev_id, qp_id, &trs_queue_conf);
+	if (rc != 0) {
+		dao_err("Could not configure ethernet transport queue.");
+		goto tx_mp_free;
+	}
+
+	rc = dao_eth_trs_dev_queue_map(dev_id, qp_id, &qp->port_id, &qp->queue_id);
+	if (rc != 0) {
+		dao_err("Could not map ethernet transport queue.");
+		goto tx_mp_free;
+	}
+
+	dev->qp[qp_id] = qp;
+
+	return 0;
+
+tx_mp_free:
+	rte_mempool_free(qp->tx_mp);
+
+rx_mp_free:
+	rte_mempool_free(qp->rx_mp);
+
+qp_free:
+	rte_free(qp);
+	return -ENOMEM;
+}
+
+static int
+liquid_crypto_qp_free(uint8_t dev_id, uint16_t qp_id)
+{
+	struct liquid_crypto_dev *dev;
+	struct liquid_crypto_qp *qp;
+
+	dev = &liquid_crypto_devs[dev_id];
+	qp = dev->qp[qp_id];
+
+	if (qp == NULL)
+		return 0;
+
+	rte_mempool_free(qp->rx_mp);
+	rte_mempool_free(qp->tx_mp);
+	rte_free(qp);
+
+	dev->qp[qp_id] = NULL;
+
+	return 0;
 }
 
 int
--- lib/liquid_crypto/liquid_crypto_priv.h
+++ lib/liquid_crypto/liquid_crypto_priv.h
@@ -5,6 +5,11 @@
 #ifndef __LIQUID_CRYPTO_PRIV_H__
 #define __LIQUID_CRYPTO_PRIV_H__
 
+#include <rte_common.h>
+#include <rte_mempool.h>
+
+#define LIQUID_CRYPTO_BUF_SZ_MIN 64ull
+
 #define LIQUID_CRYPTO_MAX_NB_QP 64
 
 /** Liquid crypto device */
@@ -15,6 +20,18 @@ struct liquid_crypto_dev {
 	uint16_t nb_qp;
 	/** Queue pair pointers */
 	void *qp[LIQUID_CRYPTO_MAX_NB_QP];
+} __rte_cache_aligned;
+
+/** Liquid crypto queue pair */
+struct liquid_crypto_qp {
+	/** Ethernet port ID */
+	uint16_t port_id;
+	/** Ethernet queue ID */
+	uint16_t queue_id;
+	/** RX mempool */
+	struct rte_mempool *rx_mp;
+	/** TX mempool */
+	struct rte_mempool *tx_mp;
 } __rte_cache_aligned;
 
 #endif /* __LIQUID_CRYPTO_PRIV_H__ */
