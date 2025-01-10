ec9bc4aa3b31636c48867b16acd2e72e8fdc1acd
Anoob Joseph <anoobj@marvell.com>
2025-01-10 16:21:39 +0000

    lib/liquid_crypto: add response structs
    Add response structs. Update the headers to have request index.
    
    Signed-off-by: Anoob Joseph <anoobj@marvell.com>
    Change-Id: I7f0e9b8abe4f624100c1edae2392ff535f762282
    Reviewed-by: Gowrishankar Muthukrishnan <gmuthukrishn@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpu-offload/+/147613
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Jerin Jacob <jerinj@marvell.com>

--- lib/liquid_crypto/lc_transport.h
+++ lib/liquid_crypto/lc_transport.h
@@ -7,21 +7,42 @@
 
 #include <rte_common.h>
 
+#include <dao_liquid_crypto.h>
+#include <dao_util.h>
+
 #include <../../lib/eth_transport/dao_eth_trs.h>
 
+struct __rte_packed __dao_lc_hdr {
+	struct dao_eth_trs_hdr trs_hdr;
+	uint32_t req_idx;
+};
+
+DAO_STATIC_ASSERT(sizeof(struct __dao_lc_hdr) == 8);
+
 struct __rte_packed __dao_lc_req_sym {
-	struct dao_eth_trs_hdr hdr;
-	uint32_t rsvd_align;
+	struct __dao_lc_hdr hdr;
 	uint64_t w4;
 	uint64_t w7;
 	uint8_t dptr[];
 };
 
+struct __rte_packed __dao_lc_resp_sym {
+	struct __dao_lc_hdr hdr;
+	union dao_cpt_res_s res;
+	uint8_t rptr[];
+};
+
 struct __rte_packed __dao_lc_req_asym {
-	struct dao_eth_trs_hdr hdr;
+	struct __dao_lc_hdr hdr;
 	uint32_t rsvd_align;
 	uint64_t w4;
 	uint8_t dptr[];
+};
+
+struct __rte_packed __dao_lc_resp_asym {
+	struct __dao_lc_hdr hdr;
+	union dao_cpt_res_s res;
+	uint8_t rptr[];
 };
 
 #endif /*  __LC_TRANSPORT_H__ */
