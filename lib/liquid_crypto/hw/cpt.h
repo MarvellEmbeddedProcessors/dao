98282e519de774e088f9dc5f5c23e622e535d4f8
Vidya Sagar Velumuri <vvelumuri@marvell.com>
2025-01-16 23:21:02 -0800

    lib/liquid_crypto: add CPT instruction definitions
    Add cpt instruction definitions.
    Add SE opcode definitions
    
    Signed-off-by: Vidya Sagar Velumuri <vvelumuri@marvell.com>
    Change-Id: Ib926e2b94b2481692cb84a57df8d841d6e71c3fb
    Reviewed-by: Anoob Joseph <anoobj@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpu-offload/+/147763
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Jerin Jacob <jerinj@marvell.com>

--- lib/liquid_crypto/hw/cpt.h
+++ lib/liquid_crypto/hw/cpt.h
@@ -5,6 +5,8 @@
 #ifndef __HW_CPT_H__
 #define __HW_CPT_H__
 
+#define ROC_ALIGN 128
+
 /* Completion codes */
 #define CPT_COMP_NOT_DONE (0x0ull)
 #define CPT_COMP_GOOD     (0x1ull)
@@ -26,6 +28,22 @@ union cpt_inst_w4 {
 	} s;
 };
 
+union cpt_inst_w5 {
+	uint64_t u64;
+	struct {
+		uint64_t dptr : 60;
+		uint64_t gather_sz : 4;
+	} s;
+};
+
+union cpt_inst_w6 {
+	uint64_t u64;
+	struct {
+		uint64_t rptr : 60;
+		uint64_t scatter_sz : 4;
+	} s;
+};
+
 union cpt_inst_w7 {
 	uint64_t u64;
 	struct {
@@ -33,6 +51,53 @@ union cpt_inst_w7 {
 		uint64_t ctx_val : 1;
 		uint64_t egrp : 3;
 	} s;
+};
+
+struct cpt_inst_s {
+	union cpt_inst_w0 {
+		struct {
+			uint64_t nixtxl : 3;
+			uint64_t doneint : 1;
+			uint64_t nixtx_addr : 60;
+		} s;
+		uint64_t u64;
+	} w0;
+
+	uint64_t res_addr;
+
+	union cpt_inst_w2 {
+		struct {
+			uint64_t tag : 32;
+			uint64_t tt : 2;
+			uint64_t grp : 10;
+			uint64_t reserved_172_175 : 4;
+			uint64_t rvu_pf_func : 16;
+		} s;
+		uint64_t u64;
+	} w2;
+
+	union cpt_inst_w3 {
+		struct {
+			uint64_t qord : 1;
+			uint64_t reserved_194_193 : 2;
+			uint64_t wqe_ptr : 61;
+		} s;
+		uint64_t u64;
+	} w3;
+
+	union cpt_inst_w4 w4;
+
+	union {
+		union cpt_inst_w5 w5;
+		uint64_t dptr;
+	};
+
+	union {
+		union cpt_inst_w6 w6;
+		uint64_t rptr;
+	};
+
+	union cpt_inst_w7 w7;
 };
 
 /* Default engine groups */
---
+++ lib/liquid_crypto/mc/se.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: Marvell-MIT
+ * Copyright (c) 2025 Marvell.
+ */
+#ifndef __MC_SE_H__
+#define __MC_SE_H__
+
+/* SE opcodes */
+#define ROC_SE_MAJOR_OP_MISC             0x01ULL
+#define ROC_SE_MISC_MINOR_OP_PASSTHROUGH 0x03ULL
+
+#endif /* __MC_SE_H__ */
