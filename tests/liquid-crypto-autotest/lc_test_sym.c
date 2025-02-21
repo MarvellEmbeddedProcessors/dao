de1475a1b7cb340f0e247dca83d443a1045c23de
Aakash Sasidharan <asasidharan@marvell.com>
2025-02-21 15:08:39 +0530

    tests/liquid-crypto: add sym encrypt tests
    Add symmetric encrypt test and verify the resulting
    data after encryption.
    
    Signed-off-by: Aakash Sasidharan <asasidharan@marvell.com>
    Change-Id: Ie59709d3d531c3ab26d25e2e38b301e72dca1017
    Reviewed-by: Anoob Joseph <anoobj@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpu-offload/+/152031
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Jerin Jacob <jerinj@marvell.com>

--- tests/liquid-crypto-autotest/lc_test_sym.c
+++ tests/liquid-crypto-autotest/lc_test_sym.c
@@ -3,6 +3,7 @@
  */
 
 #include <rte_cycles.h>
+#include <rte_hexdump.h>
 #include <rte_random.h>
 
 #include <dao_liquid_crypto.h>
@@ -43,18 +44,25 @@ sess_event_dequeue(uint8_t dev_id, struct dao_lc_cmd_event *ev)
 static int
 test_block_cipher_only(const void *data)
 {
+	uint8_t in_buf_data[MAX_PLAINTEXT_LEN] = {0};
 	const struct test_sym_params *params = data;
+	uint8_t cipher_iv[MAX_IV_LEN] = {0};
+	uint8_t dev_id = glb_params.dev_id;
+	uint16_t qp_id = glb_params.qp_id;
 	uint64_t sess_cookie = rte_rand();
+	struct dao_lc_buf in_buf[1] = {0};
+	struct dao_lc_sym_op op[1] = {0};
 	struct dao_lc_cmd_event ev;
+	struct dao_lc_res res[1];
 	int ret;
 
-	ret = dao_liquid_crypto_sym_sess_create(glb_params.dev_id, &params->ctx, sess_cookie);
+	ret = dao_liquid_crypto_sym_sess_create(dev_id, &params->ctx, sess_cookie);
 	if (ret < 0) {
 		TEST_LC_ERR("Could not create session");
 		return -1;
 	}
 
-	ret = sess_event_dequeue(glb_params.dev_id, &ev);
+	ret = sess_event_dequeue(dev_id, &ev);
 	if (ret < 0) {
 		TEST_LC_ERR("Could not dequeue session event");
 		return -1;
@@ -64,6 +72,42 @@ test_block_cipher_only(const void *data)
 	TEST_ASSERT(ev.sess_event.sess_id != DAO_LC_SESS_ID_INVALID, "Invalid session ID");
 	TEST_ASSERT(ev.sess_event.sess_cookie == sess_cookie, "Invalid operation cookie");
 
+	/* Perform encrypt operation */
+	op[0].op_cookie = sess_cookie;
+	op[0].sess_id = ev.sess_event.sess_id;
+
+	memcpy(in_buf_data, params->plaintext.data, params->plaintext.len);
+	in_buf[0].data = in_buf_data;
+	in_buf[0].seg_len = params->plaintext.len;
+	in_buf[0].total_len = params->plaintext.len;
+
+	op[0].in_buffer = in_buf;
+	op[0].cipher_offset = 0;
+	op[0].cipher_len = params->ciphertext.len;
+
+	memcpy(cipher_iv, params->iv.data, params->iv.len);
+	op[0].cipher_iv = cipher_iv;
+	op[0].encrypt = 1;
+
+	ret = dao_liquid_crypto_sym_enqueue_burst(dev_id, qp_id, op, 1);
+	if (ret < 0) {
+		TEST_LC_ERR("Could not enqueue encrypt operation");
+		return -1;
+	}
+
+	ret = op_dequeue(dev_id, qp_id, res);
+	if (ret < 0) {
+		TEST_LC_ERR("Could not dequeue encrypt operation");
+		return -1;
+	}
+
+	ret = memcmp(in_buf_data, params->ciphertext.data, params->ciphertext.len);
+	if (ret != 0) {
+		rte_hexdump(stdout, "RESULT: ", in_buf_data, params->ciphertext.len);
+		rte_hexdump(stdout, "EXPECTED: ", params->ciphertext.data, params->ciphertext.len);
+	}
+	TEST_ASSERT(ret == 0, "Invalid result");
+
 	ret = dao_liquid_crypto_sym_sess_destroy(glb_params.dev_id, ev.sess_event.sess_id,
 						 sess_cookie);
 	if (ret < 0) {
@@ -71,7 +115,7 @@ test_block_cipher_only(const void *data)
 		return -1;
 	}
 
-	ret = sess_event_dequeue(glb_params.dev_id, &ev);
+	ret = sess_event_dequeue(dev_id, &ev);
 	if (ret < 0) {
 		TEST_LC_ERR("Could not dequeue session event");
 		return -1;
