4298e327fb6238533539cd57590d6c5548b82961
Nitin Saxena <nsaxena@marvell.com>
2025-03-25 15:13:14 +0530

    bfs: fix copying of ld-linux-aarch64.so.1
    Instead of creating symlink, copied ld-linux-aatch64.so.1 from
    toolchain directory to staging directory
    
    Signed-off-by: Nitin Saxena <nsaxena@marvell.com>
    Change-Id: I564aa506f496fbdab6118b0a97b145de231e2330
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpu-offload/+/149214
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>

--- bfs/board/toolchain-fixup.sh
+++ bfs/board/toolchain-fixup.sh
@@ -4,13 +4,17 @@
 #!/bin/sh
 
 set -x
-STAGING_DIR=$1
-TOOLCHAIN_EXTERNAL_PATH=$2
-rm -rf ${STAGING_DIR}/lib/ld-linux-aarch64.so.1
-if [ -e ${HOST_DIR}/opt/ext-toolchain/aarch64-marvell-linux-gnu/sys-root/lib/ld-linux-aarch64.so.1 ]; then
-ln -sf ${HOST_DIR}/opt/ext-toolchain/aarch64-marvell-linux-gnu/sys-root/lib/ld-linux-aarch64.so.1 ${STAGING_DIR}/lib/ld-linux-aarch64.so.1
+FILENAME=ld-linux-aarch64.so.1
+STAGING_DIR=$1/lib
+TOOLCHAIN_EXTERNAL_PATH=$2/aarch64-marvell-linux-gnu/sys-root/lib
+HOST_DIR_PATH=${HOST_DIR}/opt/ext-toolchain/aarch64-marvell-linux-gnu/sys-root/lib
+rm -rf ${STAGING_DIR}/${FILENAME}
+if [ -e ${HOST_DIR_PATH}/${FILENAME} ]; then
+echo "__DAO_BFS_TOOLCHAIN_FIXUP__: Copied ${HOST_DIR_PATH}/${FILENAME} -> ${STAGING_DIR}/"
+cp -fp ${HOST_DIR_PATH}/${FILENAME} ${STAGING_DIR}/${FILENAME}
 fi
 
-if [ -e ${TOOLCHAIN_EXTERNAL_PATH}/aarch64-marvell-linux-gnu/sys-root/lib/ld-linux-aarch64.so.1 ]; then
-ln -sf ${TOOLCHAIN_EXTERNAL_PATH}/aarch64-marvell-linux-gnu/sys-root/lib/ld-linux-aarch64.so.1 ${STAGING_DIR}/lib/ld-linux-aarch64.so.1
+if [ -e ${TOOLCHAIN_EXTERNAL_PATH}/${FILENAME} ]; then
+echo "__DAO_BFS_TOOLCHAIN_FIXUP__: Copied $TOOLCHAIN_EXTERNAL_PATH/${FILENAME} -> $STAGING_DIR/"
+cp -fp ${TOOLCHAIN_EXTERNAL_PATH}/${FILENAME} ${STAGING_DIR}/${FILENAME}
 fi
