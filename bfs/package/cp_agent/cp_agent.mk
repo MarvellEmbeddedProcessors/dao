947ba16b63b7b6ac316ca5003a00529bfea76ed6
Vidya Sagar Velumuri <vvelumuri@marvell.com>
2025-04-08 01:32:35 -0700

    bfs: add cp agent package to buildroot
    Add cp agent as a package in buildroot
    
    Signed-off-by: Vidya Sagar Velumuri <vvelumuri@marvell.com>
    Change-Id: I2095090a04e501b5518328973b49733c85cdedd8
    Reviewed-by: Anoob Joseph <anoobj@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpu-offload/+/154242
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Jerin Jacob <jerinj@marvell.com>
    Reviewed-by: Nitin Saxena <nsaxena@marvell.com>

---
+++ bfs/package/cp_agent/cp_agent.mk
@@ -0,0 +1,37 @@
+# SPDX-License-Identifier: Marvell-MIT
+# Copyright (c) 2025 Marvell.
+
+################################################################################
+# CP AGENT
+################################################################################
+
+CP_AGENT_VERSION = pcie_ep_octeon_target-release
+CP_AGENT_SITE = https://github.com/MarvellEmbeddedProcessors/pcie_ep_octeon_target.git
+CP_AGENT_SITE_METHOD = git
+CP_AGENT_SOURCE = cp_agent-$(CP_AGENT_VERSION).tar.gz
+
+CP_AGENT_INSTALL_STAGING = YES
+CP_AGENT_LICENSE = \
+	BSD-3-Clause, \
+	MIT
+
+CP_AGENT_DEPENDENCIES = libconfig
+
+CP_AGENT_CONFIG = $(call qstrip,$(BR2_PACKAGE_CP_AGENT_CONFIG))
+CP_AGENT_CONF_OPTS= --host=aarch64-marvell-linux-gnu --prefix=$(TARGET_DIR)/usr
+
+define CP_AGENT_BUILD_CMDS
+	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)/target/libs/octep_cp_lib
+
+	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)/target/apps/octep_cp_agent \
+		CFLAGS="-I$(STAGING_DIR)/usr/include -I$(LIB_DIR)/bin/include -I$(@D)/target/libs/octep_cp_lib/bin/include" \
+		LDFLAGS="-L$(LIB_DIR)/ -L$(STAGING_DIR)/usr/lib -L$(@D)/target/libs/octep_cp_lib/bin/lib"
+endef
+
+define CP_AGENT_INSTALL_TARGET_CMDS
+	$(INSTALL) $(@D)/target/apps/octep_cp_agent/bin/bin/* $(TARGET_DIR)/usr/bin/
+	$(INSTALL) $(@D)/target/libs/octep_cp_lib/bin/lib/* $(TARGET_DIR)/usr/lib/
+	$(INSTALL) $(@D)/target/apps/octep_cp_agent/*.cfg $(TARGET_DIR)/etc/
+endef
+
+$(eval $(generic-package))
