35c9805edb8752320bc1fa72dba9202e2091da27
Anoob Joseph <anoobj@marvell.com>
2025-04-08 11:30:07 +0000

    bfs: remove unused DPDK & DAO bins
    Remove unused DPDK & DAO binaries.
    
    Signed-off-by: Anoob Joseph <anoobj@marvell.com>
    Change-Id: Ia283e6ca9c0f9f788f00c72dd80486a5a85eca5c
    Reviewed-by: Vamsi Krishna Attunuru <vattunuru@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpu-offload/+/150268
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>

--- bfs/board/octeon_cn9k/lc/build_scripts/post-build.sh
+++ bfs/board/octeon_cn9k/lc/build_scripts/post-build.sh
@@ -13,3 +13,17 @@ for file in "${files[@]}"; do
 		mv "${TARGET_DIR}/usr/bin/$file" "${TARGET_DIR}/root/lc_service/bin/$file"
 	fi
 done
+
+# Remove additional DPDK test files
+dpdk_files=("dpdk-test*"
+	    "dpdk-graph"
+	    "dpdk-proc-info"
+	    "dpdk-dumpcap")
+
+for file in "${dpdk_files[@]}"; do
+	eval rm -f "${TARGET_DIR}/usr/bin/$file"
+done
+
+# Remove additional DAO apps
+pattern="${TARGET_DIR}/usr/bin/dao*"
+eval rm -f "$pattern"
