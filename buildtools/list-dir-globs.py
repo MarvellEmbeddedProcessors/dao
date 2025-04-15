f2b662ff6595dd30dbfb208ce1b1c76a5ba506e3
Anoob Joseph <anoobj@marvell.com>
2025-04-15 12:30:13 +0000

    build: add ability to enable and disable specific libs
    Add ability to enable and disable specific libs.
    
    Signed-off-by: Anoob Joseph <anoobj@marvell.com>
    Change-Id: Ibf0606445191885c6adcf18b6169e428a875a65c
    Reviewed-by: Jerin Jacob <jerinj@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpu-offload/+/152071
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>

---
+++ buildtools/list-dir-globs.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-License-Identifier: Marvell-MIT
+# Copyright(c) 2020 Intel Corporation
+# Copyright(c) 2025 Marvell.
+
+# File based on https://github.com/DPDK/dpdk/blob/main/buildtools/list-dir-globs.py
+
+import sys
+import os
+from glob import iglob
+
+if len(sys.argv) != 2:
+    print("Usage: {0} <path-glob>[,<path-glob>[,...]]".format(sys.argv[0]))
+    sys.exit(1)
+
+root = os.path.join(os.getenv('MESON_SOURCE_ROOT', '.'),
+                    os.getenv('MESON_SUBDIR', '.'))
+
+for path in sys.argv[1].split(','):
+    if path:
+        for p in iglob(os.path.join(root, path)):
+            if os.path.isdir(p):
+                print(os.path.relpath(p, start=root).replace('\\', '/'))
