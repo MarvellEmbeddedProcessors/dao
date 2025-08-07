a778af59b809b601fda11cb1286e1704ea243bd5
Rupesh Chiluka <rchiluka@marvell.com>
2025-08-07 15:11:21 +0530

    lib/liquid_crypto: add SHA3 support
    Add SHA3-224/256/384/512 support
    
    Signed-off-by: Rupesh Chiluka <rchiluka@marvell.com>
    Change-Id: I59eb90dfaca16bd2b0fa4139aa7aa09c5ca29b5f
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpu-offload/+/159796
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Tejasree Kondoj <ktejasree@marvell.com>
    Reviewed-by: Anoob Joseph <anoobj@marvell.com>
    Reviewed-by: sa_ip-sw-jenkins1

--- lib/liquid_crypto/liquid_crypto_sym.c
+++ lib/liquid_crypto/liquid_crypto_sym.c
@@ -125,6 +125,22 @@ sym_sess_hash_digest_len_validate(const struct dao_lc_sym_ctx *ctx)
 		if (ctx->fc.mac_len == 64)
 			return 0;
 		break;
+	case DAO_LC_HASH_TYPE_SHA3_SHA224:
+		if (ctx->fc.mac_len == 28)
+			return 0;
+		break;
+	case DAO_LC_HASH_TYPE_SHA3_SHA256:
+		if (ctx->fc.mac_len == 32)
+			return 0;
+		break;
+	case DAO_LC_HASH_TYPE_SHA3_SHA384:
+		if (ctx->fc.mac_len == 48)
+			return 0;
+		break;
+	case DAO_LC_HASH_TYPE_SHA3_SHA512:
+		if (ctx->fc.mac_len == 64)
+			return 0;
+		break;
 	default:
 		dao_err("Unsupported hash type.");
 		return -ENOTSUP;
@@ -394,6 +410,10 @@ sym_sess_hash_verify(const struct dao_lc_sym_ctx *ctx)
 		case DAO_LC_HASH_TYPE_SHA2_SHA256:
 		case DAO_LC_HASH_TYPE_SHA2_SHA384:
 		case DAO_LC_HASH_TYPE_SHA2_SHA512:
+		case DAO_LC_HASH_TYPE_SHA3_SHA224:
+		case DAO_LC_HASH_TYPE_SHA3_SHA256:
+		case DAO_LC_HASH_TYPE_SHA3_SHA384:
+		case DAO_LC_HASH_TYPE_SHA3_SHA512:
 			break;
 		default:
 			dao_err("Unsupported hash type.");
