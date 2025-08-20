65eeb4f24a55095f223b280111c004b7c41427e1
Vidya Sagar Velumuri <vvelumuri@marvell.com>
2025-08-20 00:03:28 -0700

    lib/liquid-crypto: bump version to 25.08-rc0
    Bump version to 25.08-rc0
    
    Signed-off-by: Vidya Sagar Velumuri <vvelumuri@marvell.com>
    Change-Id: If54fe437c18c9b762d790ee3bd3f7bbb5af5c344
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpu-offload/+/160827
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: sa_ip-sw-jenkins1
    Reviewed-by: Anoob Joseph <anoobj@marvell.com>

--- lib/liquid_crypto/dao_liquid_crypto.h
+++ lib/liquid_crypto/dao_liquid_crypto.h
@@ -19,7 +19,7 @@
 #include "liquid_crypto_asym.h"
 
 /** The version of the liquid crypto library */
-#define DAO_LC_VERSION "25.07.1"
+#define DAO_LC_VERSION "25.08.0"
 /** The maximum length of the version string. */
 #define DAO_CRYPTO_VERSION_LEN 32
 /** The maximum number of devices supported by the liquid crypto library. */
