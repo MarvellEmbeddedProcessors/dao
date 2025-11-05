1f17bc0b0a28bba81639d337708dcb21e9ba742c
Rupesh Chiluka <rchiluka@marvell.com>
2025-11-05 11:18:57 +0530

    tests/liquid_crypto: add zero length test cases for AEAD
    Add zero length test cases for AEAD
    
    Signed-off-by: Rupesh Chiluka <rchiluka@marvell.com>
    Change-Id: I7a7cd4a663eb16e3c746458995c06ea4d2f4832f
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpu-offload/+/166101
    Reviewed-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Tejasree Kondoj <ktejasree@marvell.com>
    Reviewed-by: Anoob Joseph <anoobj@marvell.com>
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>

--- tests/liquid-crypto-autotest/lc_test_sym_aes.h
+++ tests/liquid-crypto-autotest/lc_test_sym_aes.h
@@ -7,6 +7,8 @@
 
 #include "lc_test_sym.h"
 
+static const uint8_t empty_data[] = {};
+
 static const uint8_t plaintext_aes_common[] = {
 	"What a lousy earth! He wondered how many people "
 	"were destitute that same night even in his own "
@@ -408,6 +410,51 @@ static struct test_sym_params aes_gcm_128_test_data = {
 	},
 };
 
+static const uint8_t digest_aes128gcm_empty[] = {
+	0x32, 0x47, 0x18, 0x4b, 0x3c, 0x4f, 0x69, 0xa4,
+	0x4d, 0xbc, 0xd2, 0x28, 0x87, 0xbb, 0xb4, 0x18
+};
+
+static struct test_sym_params aes_gcm_128_empty_test_data = {
+	.ctx = {
+		.opcode = DAO_LC_SYM_OPCODE_FC,
+		.fc = {
+			.iv_source = DAO_LC_FC_IV_SRC_OP,
+			.aes_key_len = DAO_LC_FC_AES_KEY_LEN_128,
+			.enc_cipher = DAO_LC_FC_ENC_CIPHER_AES_GCM,
+			.mac_len = 16,
+			.encr_key = {
+				0xfe, 0xff, 0xe9, 0x92, 0x86, 0x65, 0x73, 0x1c,
+				0x6d, 0x6a, 0x8f, 0x94, 0x67, 0x30, 0x83, 0x08
+			},
+
+		},
+	},
+	.iv = {
+		.data = {
+			0xca, 0xfe, 0xba, 0xbe, 0xfa, 0xce, 0xdb, 0xad,
+			0xde, 0xca, 0xf8, 0x88,
+		},
+		.len = 12
+	},
+	.plaintext = {
+		.data = empty_data,
+		.len = 0
+	},
+	.ciphertext = {
+		.data = empty_data,
+		.len = 0
+	},
+	.aad = {
+		.data = empty_data,
+		.len = 0
+	},
+	 .digest = {
+		.data = digest_aes128gcm_empty,
+		.len = 16
+	},
+};
+
 static const uint8_t digest_aes192gcm[] = {
 	0xC2, 0xD8, 0x4C, 0x6B, 0xA8, 0x3B, 0xA5, 0x6B,
 	0x18, 0x9F, 0xE6, 0xEF, 0x66, 0x24, 0xDD, 0xDA
@@ -461,6 +508,52 @@ static struct test_sym_params aes_gcm_192_test_data = {
 	},
 	 .digest = {
 		.data = digest_aes192gcm,
+		.len = 16
+	},
+};
+
+static const uint8_t digest_aes192gcm_empty[] = {
+	0xc8, 0x35, 0xaa, 0x88, 0xae, 0xbb, 0xc9, 0x4f,
+	0x5a, 0x02, 0xe1, 0x79, 0xfd, 0xcf, 0xc3, 0xe4
+};
+
+static struct test_sym_params aes_gcm_192_empty_test_data = {
+	.ctx = {
+		.opcode = DAO_LC_SYM_OPCODE_FC,
+		.fc = {
+			.iv_source = DAO_LC_FC_IV_SRC_OP,
+			.aes_key_len = DAO_LC_FC_AES_KEY_LEN_192,
+			.enc_cipher = DAO_LC_FC_ENC_CIPHER_AES_GCM,
+			.mac_len = 16,
+			.encr_key = {
+				0xfe, 0xff, 0xe9, 0x92, 0x86, 0x65, 0x73, 0x1c,
+				0x6d, 0x6a, 0x8f, 0x94, 0x67, 0x30, 0x83, 0x08,
+				0xFE, 0xFF, 0xE9, 0x92, 0x86, 0x65, 0x73, 0x1C
+			},
+
+		},
+	},
+	.iv = {
+		.data = {
+			0xca, 0xfe, 0xba, 0xbe, 0xfa, 0xce, 0xdb, 0xad,
+			0xde, 0xca, 0xf8, 0x88,
+		},
+		.len = 12
+	},
+	.plaintext = {
+		.data = empty_data,
+		.len = 0
+	},
+	.ciphertext = {
+		.data = empty_data,
+		.len = 0
+	},
+	.aad = {
+		.data = empty_data,
+		.len = 0
+	},
+	 .digest = {
+		.data = digest_aes192gcm_empty,
 		.len = 16
 	},
 };
@@ -519,6 +612,53 @@ static struct test_sym_params aes_gcm_256_test_data = {
 	},
 	 .digest = {
 		.data = digest_aes256gcm,
+		.len = 16
+	},
+};
+
+static const uint8_t digest_aes256gcm_empty[] = {
+	0x0d, 0xbb, 0xc0, 0xd0, 0x70, 0x26, 0xf6, 0x8c,
+	0x0c, 0x40, 0x02, 0x54, 0x2b, 0xf6, 0x55, 0xcd
+};
+
+static struct test_sym_params aes_gcm_256_empty_test_data = {
+	.ctx = {
+		.opcode = DAO_LC_SYM_OPCODE_FC,
+		.fc = {
+			.iv_source = DAO_LC_FC_IV_SRC_OP,
+			.aes_key_len = DAO_LC_FC_AES_KEY_LEN_256,
+			.enc_cipher = DAO_LC_FC_ENC_CIPHER_AES_GCM,
+			.mac_len = 16,
+			.encr_key = {
+				0xfe, 0xff, 0xe9, 0x92, 0x86, 0x65, 0x73, 0x1c,
+				0x6d, 0x6a, 0x8f, 0x94, 0x67, 0x30, 0x83, 0x08,
+				0xd9, 0x31, 0x32, 0x25, 0xf8, 0x84, 0x06, 0xe5,
+				0xa5, 0x59, 0x09, 0xc5, 0xaf, 0xf5, 0x26, 0x9a
+			},
+
+		},
+	},
+	.iv = {
+		.data = {
+			0xca, 0xfe, 0xba, 0xbe, 0xfa, 0xce, 0xdb, 0xad,
+			0xde, 0xca, 0xf8, 0x88,
+		},
+		.len = 12
+	},
+	.plaintext = {
+		.data = empty_data,
+		.len = 0
+	},
+	.ciphertext = {
+		.data = empty_data,
+		.len = 0
+	},
+	.aad = {
+		.data = empty_data,
+		.len = 0
+	},
+	 .digest = {
+		.data = digest_aes256gcm_empty,
 		.len = 16
 	},
 };
@@ -702,6 +842,50 @@ static struct test_sym_params aes_ccm_128_test_data = {
 	},
 	 .digest = {
 		.data = digest_aes128ccm,
+		.len = 8
+	},
+};
+
+static const uint8_t digest_aes128ccm_empty[] = {
+	0xd1, 0x93, 0xea, 0x25, 0x37, 0xb1, 0xc1, 0x84
+};
+
+static struct test_sym_params aes_ccm_128_empty_test_data = {
+	.ctx = {
+		.opcode = DAO_LC_SYM_OPCODE_FC,
+		.fc = {
+			.iv_source = DAO_LC_FC_IV_SRC_OP,
+			.aes_key_len = DAO_LC_FC_AES_KEY_LEN_128,
+			.enc_cipher = DAO_LC_FC_ENC_CIPHER_AES_CCM,
+			.mac_len = 8,
+			.encr_key = {
+				0xc9, 0x7c, 0x1f, 0x67, 0xce, 0x37, 0x11, 0x85,
+				0x51, 0x4a, 0x8a, 0x19, 0xf2, 0xbd, 0xd5, 0x2f
+			},
+
+		},
+	},
+	.iv = {
+		.data = {
+			0x00, 0x50, 0x30, 0xf1, 0x84, 0x44, 0x08, 0xb5,
+			0x03, 0x97, 0x76, 0xe7, 0x0c
+		},
+		.len = 13
+	},
+	.plaintext = {
+		.data = empty_data,
+		.len = 0
+	},
+	.ciphertext = {
+		.data = empty_data,
+		.len = 0
+	},
+	.aad = {
+		.data = empty_data,
+		.len = 0
+	},
+	 .digest = {
+		.data = digest_aes128ccm_empty,
 		.len = 8
 	},
 };
@@ -932,6 +1116,51 @@ static struct test_sym_params aes_ccm_192_test_data = {
 	},
 };
 
+static const uint8_t digest_aes192ccm_empty[] = {
+	0x15, 0x06, 0x98, 0x6e, 0x35, 0x5b, 0xc3, 0x46
+};
+
+static struct test_sym_params aes_ccm_192_empty_test_data = {
+	.ctx = {
+		.opcode = DAO_LC_SYM_OPCODE_FC,
+		.fc = {
+			.iv_source = DAO_LC_FC_IV_SRC_OP,
+			.aes_key_len = DAO_LC_FC_AES_KEY_LEN_192,
+			.enc_cipher = DAO_LC_FC_ENC_CIPHER_AES_CCM,
+			.mac_len = 8,
+			.encr_key = {
+				0xc9, 0x7c, 0x1f, 0x67, 0xce, 0x37, 0x11, 0x85,
+				0x51, 0x4a, 0x8a, 0x19, 0xf2, 0xbd, 0xd5, 0x2f,
+				0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57
+			},
+
+		},
+	},
+	.iv = {
+		.data = {
+			0x00, 0x50, 0x30, 0xf1, 0x84, 0x44, 0x08,
+			0xb5, 0x03, 0x97, 0x76, 0xe7, 0x0c
+		},
+		.len = 13
+	},
+	.plaintext = {
+		.data = empty_data,
+		.len = 0
+	},
+	.ciphertext = {
+		.data = empty_data,
+		.len = 0
+	},
+	.aad = {
+		.data = empty_data,
+		.len = 0
+	},
+	 .digest = {
+		.data = digest_aes192ccm_empty,
+		.len = 8
+	},
+};
+
 static const uint8_t digest_aes256ccm[] = {
 	0xab, 0x02, 0xe9, 0xd1, 0x16, 0x69, 0xed, 0x0a
 };
@@ -980,6 +1209,52 @@ static struct test_sym_params aes_ccm_256_test_data = {
 	},
 	 .digest = {
 		.data = digest_aes256ccm,
+		.len = 8
+	},
+};
+
+static const uint8_t digest_aes256ccm_empty[] = {
+	0xfa, 0x82, 0x77, 0x48, 0xe9, 0x3a, 0xc4, 0x8a
+};
+
+static struct test_sym_params aes_ccm_256_empty_test_data = {
+	.ctx = {
+		.opcode = DAO_LC_SYM_OPCODE_FC,
+		.fc = {
+			.iv_source = DAO_LC_FC_IV_SRC_OP,
+			.aes_key_len = DAO_LC_FC_AES_KEY_LEN_256,
+			.enc_cipher = DAO_LC_FC_ENC_CIPHER_AES_CCM,
+			.mac_len = 8,
+			.encr_key = {
+				0xc9, 0x7c, 0x1f, 0x67, 0xce, 0x37, 0x11, 0x85,
+				0x51, 0x4a, 0x8a, 0x19, 0xf2, 0xbd, 0xd5, 0x2f,
+				0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57,
+				0x58, 0x59, 0x5a, 0x5b, 0x5c, 0x5d, 0x5e, 0x5f
+			},
+
+		},
+	},
+	.iv = {
+		.data = {
+			0x00, 0x50, 0x30, 0xf1, 0x84, 0x44, 0x08,
+			0xb5, 0x03, 0x97, 0x76, 0xe7, 0x0c
+		},
+		.len = 13
+	},
+	.plaintext = {
+		.data = empty_data,
+		.len = 0
+	},
+	.ciphertext = {
+		.data = empty_data,
+		.len = 0
+	},
+	.aad = {
+		.data = empty_data,
+		.len = 0
+	},
+	 .digest = {
+		.data = digest_aes256ccm_empty,
 		.len = 8
 	},
 };
@@ -1067,6 +1342,52 @@ static struct test_sym_params chacha_poly_test_data = {
 	},
 	.digest = {
 		.data = digest_chacha_poly,
+		.len = 16
+	},
+};
+
+static const uint8_t digest_chacha_poly_empty[] = {
+	0xa0, 0x78, 0x4d, 0x7a, 0x47, 0x16, 0xf3, 0xfe,
+	0xb4, 0xf6, 0x4e, 0x7f, 0x4b, 0x39, 0xbf, 0x04
+};
+
+static struct test_sym_params chacha_poly_empty_test_data = {
+	.ctx = {
+		.opcode = DAO_LC_SYM_OPCODE_FC,
+		.fc = {
+			.iv_source = DAO_LC_FC_IV_SRC_OP,
+			.enc_cipher = DAO_LC_FC_ENC_CIPHER_CHACHA,
+			.hash_type = DAO_LC_HASH_TYPE_POLY1305,
+			.mac_len = 16,
+			.encr_key = {
+				0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87,
+				0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f,
+				0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97,
+				0x98, 0x99, 0x9a, 0x9b, 0x9c, 0x9d, 0x9e, 0x9f
+			},
+		},
+	},
+	.iv = {
+		.data = {
+			0x07, 0x00, 0x00, 0x00, 0x40, 0x41, 0x42, 0x43,
+			0x44, 0x45, 0x46, 0x47
+		},
+		.len = 12
+	},
+	.plaintext = {
+		.data = empty_data,
+		.len = 0
+	},
+	.ciphertext = {
+		.data = empty_data,
+		.len = 0
+	},
+	.aad = {
+		.data = empty_data,
+		.len = 0
+	},
+	.digest = {
+		.data = digest_chacha_poly_empty,
 		.len = 16
 	},
 };
