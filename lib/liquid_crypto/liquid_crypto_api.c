MarvellEmbeddedProcessors/dao

commit 49585993802f9874d1aa2cba5c75b91bb8d0d824

lib/liquid_crypto: change length and offset types to uint32_t
Change length and offset types to uint32_t

Signed-off-by: Rupesh Chiluka <rchiluka@marvell.com>
Change-Id: Ic2e39aa2ba8b3686b7e32d25a5d6c9aaf3ea808b
Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpu-offload/+/167652
Reviewed-by: Tejasree Kondoj <ktejasree@marvell.com>
Reviewed-by: Anoob Joseph <anoobj@marvell.com>
Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>

lib/liquid_crypto/liquid_crypto_api.c
@@ -1744,13 +1744,13 @@ dao_lc_post_process_asym(struct liquid_crypto_inflight_req *req, struct dao_lc_r
 	}
 }
 
-static inline uint16_t
-dao_lc_buf_copy_to_offset_from_mem(uint8_t *src, struct dao_lc_buf *dst, uint16_t offset,
+static inline uint32_t
+dao_lc_buf_copy_to_offset_from_mem(uint8_t *src, struct dao_lc_buf *dst, uint32_t offset,
 				   uint32_t len)
 {
 	struct dao_lc_buf *tmp = dst;
-	uint16_t copied = 0;
-	uint16_t to_copy;
+	uint32_t copied = 0;
+	uint32_t to_copy;
 
 	if (len == 0)
 		return 0;
@@ -1781,12 +1781,12 @@ dao_lc_buf_copy_to_offset_from_mem(uint8_t *src, struct dao_lc_buf *dst, uint16_
 	return copied;
 }
 
-static inline uint16_t
+static inline uint32_t
 dao_lc_buf_copy_from_mem(uint8_t *src, struct dao_lc_buf *dst, uint32_t len)
 {
 	struct dao_lc_buf *tmp = dst;
-	uint16_t copied = 0;
-	uint16_t to_copy;
+	uint32_t copied = 0;
+	uint32_t to_copy;
 
 	do {
 		to_copy = RTE_MIN(tmp->frag_len, len - copied);
@@ -1803,7 +1803,7 @@ static inline void
 dao_lc_post_process_sym(struct liquid_crypto_inflight_req *req, struct dao_lc_res *res,
 			struct rte_mbuf *mbuf)
 {
-	uint16_t result_offset, result_len, lc_buf_offset, copied;
+	uint32_t result_offset, result_len, lc_buf_offset, copied;
 	struct __dao_lc_resp_sym *resp;
 
 	resp = rte_pktmbuf_mtod(mbuf, struct __dao_lc_resp_sym *);
@@ -1833,7 +1833,7 @@ dao_lc_post_process_sym(struct liquid_crypto_inflight_req *req, struct dao_lc_re
 			/* The length is stored in big-endian format, so convert it
 			 */
 			result_len =
-				rte_be_to_cpu_16(*(uint16_t *)(resp->rptr + result_offset +
+				rte_be_to_cpu_32(*(uint32_t *)(resp->rptr + result_offset +
 							       (req->wrap_unwrap_key_len - 2)));
 
 		copied = dao_lc_buf_copy_to_offset_from_mem(
@@ -1878,8 +1878,8 @@ dao_lc_buf_copy_from_offset_to_mem(struct dao_lc_buf *src, uint8_t *dst, uint32_
 				   uint32_t len, bool is_zero_len_allowed)
 {
 	struct dao_lc_buf *tmp = src;
-	uint16_t copied = 0;
-	uint16_t to_copy;
+	uint32_t copied = 0;
+	uint32_t to_copy;
 
 	/* Skip to the offset */
 	while (tmp && offset >= tmp->frag_len) {

commit 3346350767e05554c07419330b060413e76c3613

lib/liquid_crypto: revert accidental change in key wrap
Revert accidental uint16_t to uint32_t change in key
wrap post processing

Fixes: 49585993802f ("lib/liquid_crypto: change length
and offset types to uint32_t")

Signed-off-by: Rupesh Chiluka <rchiluka@marvell.com>
Change-Id: I2e7222922e5788b732196c3540e171aa3240fd75
Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpu-offload/+/167834
Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
Reviewed-by: Anoob Joseph <anoobj@marvell.com>

lib/liquid_crypto/liquid_crypto_api.c
@@ -1833,7 +1833,7 @@ dao_lc_post_process_sym(struct liquid_crypto_inflight_req *req, struct dao_lc_re
 			/* The length is stored in big-endian format, so convert it
 			 */
 			result_len =
-				rte_be_to_cpu_32(*(uint32_t *)(resp->rptr + result_offset +
+				rte_be_to_cpu_16(*(uint16_t *)(resp->rptr + result_offset +
 							       (req->wrap_unwrap_key_len - 2)));
 
 		copied = dao_lc_buf_copy_to_offset_from_mem(