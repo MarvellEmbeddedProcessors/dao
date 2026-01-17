09d2bbc00ff12136cf8b2cf9aaf8928593500c3c
Sucharitha Sarananaga <ssarananaga@marvell.com>
2026-01-17 05:30:55 -0500

    lib/liquid-crypto: zero-pad ECDSA digest buffers to aligned size
    - In ECDSA sign and verify enqueue paths:
      - After copying the digest, explicitly zero-fill the remaining bytes
        up to `m_align`.
      - Ensures non 8-byte aligned digest lengths do not contain random
        leftover data, preventing verify mismatches.
      - Fixes inconsistencies observed when padded bytes were non-zero.
    
    Signed-off-by: Sucharitha Sarananaga <ssarananaga@marvell.com>

--- lib/liquid_crypto/liquid_crypto_api.c
+++ lib/liquid_crypto/liquid_crypto_api.c
@@ -3693,6 +3693,7 @@ dao_liquid_crypto_enq_op_ecdsa_sign(uint8_t dev_id, uint16_t qp_id,
 	dptr += p_align;
 
 	memcpy(dptr, digest, digest_len);
+	memset(dptr + digest_len, 0, m_align - digest_len);
 	dptr += m_align;
 
 	rc = rte_eth_tx_burst(qp->port_id, qp->queue_id, &mbuf, 1);
@@ -3918,6 +3919,7 @@ dao_liquid_crypto_enq_op_ecdsa_verify(uint8_t dev_id, uint16_t qp_id,
 	dptr += p_align;
 
 	memcpy(dptr, digest, digest_len);
+	memset(dptr + digest_len, 0, m_align - digest_len);
 	dptr += m_align;
 
 	/* Skip Order */
